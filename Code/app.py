6ec739b08efed31650f074b79874874c290df64f
alalalalaki <harlan.zhu@gmail.com>
2021-01-31 17:46:06 +0900

    Update journal info  and clean authors in data.

--- Code/app.py
+++ Code/app.py
@@ -12,16 +12,19 @@ st.set_page_config(page_title=None, page_icon=None, layout='centered', initial_s
 
 @st.cache
 def load_data():
-    df = pd.read_csv("../Data/article.csv")
+    df = pd.read_csv("../Data/papers.csv",
+                     usecols=["title", "authors", "abstract", "url", "jel", "journal", "year"],
+                     dtype={"year": "Int16"}
+                     )
     return df
 
 
 def show_papers(df):
     for index, row in df.iterrows():
-        st.markdown(f'{index+1}.  [{row.title}]({row.article_url}). {row.journ}. {row.year}.')
+        st.markdown(f'{index+1}.  [{row.title}]({row.url}). {row.authors}. {row.journal}. {row.year}.')
 
 
-def search_keywords(key_words, journals, year_begin, year_end, sort_mth, max_show, data_load_state):
+def search_keywords(button_clicked, df, key_words, journals, year_begin, year_end, sort_mth, max_show, data_load_state):
     if button_clicked:
         data_load_state.markdown('Searching paper...')
         if (' ' in key_words) & ("\"" not in key_words):
@@ -29,7 +32,7 @@ def search_keywords(key_words, journals, year_begin, year_end, sort_mth, max_sho
             # key_words = ''.join([f'(?=.*{i})' for i in key_words_list])
         else:
             key_words = [key_words]
-        mask_jounral = df.journ.isin(journals)
+        mask_jounral = df.journal.isin(journals)
         mask_year = (df.year >= year_begin) & (df.year <= year_end)
         dt = df.loc[mask_jounral & mask_year]
         info = dt.title + ' ' + dt.abstract.fillna('')
@@ -68,12 +71,25 @@ def sidebar_info():
     aejmac: AEJ Macroeconomics<br>
     aer: American Economic Review<br>
     aeri: AER Insights<br>
+    are: Annual Review of Economics<br>
     ecta: Econometrica<br>
+    ehr: Economic History Review<br>
+    ej: Economic Journal<br>
+    eer: European Economic Review<br>
+    eeh: Explorations in Economic History<br>
+    jde: Journal of Development Economics<br>
     jeea: Journal of the European Economic Association<br>
+    jel: The Journal of Economic History<br>
+    jel: Journal of Economic Literature<br>
     jep: Journal of Economic Perspectives<br>
+    jie: Journal of International Economics<br>
+    jole: Journal of Labor Economics<br>
+    jme: Journal of Monetary Economics<br>
     jpe: Journal of Political Economy<br>
-    pandp: AEA Papers and Proceedings<br>
+    jpube: Journal of Public Economics<br>
     qje: Quarterly Journal of Economics<br>
+    rand: RAND Journal of Economics<br>
+    red: Review of Economic Dynamics<br>
     restud: Review of Economic Studies<br>
     restat: Review of Economics and Statistics<br>
     </div>
@@ -83,22 +99,30 @@ def sidebar_info():
 def main():
     sidebar_info()
 
+    df = load_data()
+    year_min = df.year.min()
+    year_max = df.year.max()
+
     key_words = st.text_input('Keywords in Title and Abstract')
-    js = ['aer', 'jpe', 'qje', 'ecta', 'restud', 'aejmac', 'aejmic', 'aejapp', 'aejpol', 'aeri', 'restat', 'jeea', 'jep']
-    journals = st.multiselect("Journals", js, js)
+    js = ['aer', 'jpe', 'qje', 'ecta', 'restud',
+          'aejmac', 'aejmic', 'aejapp', 'aejpol', 'aeri',
+          'restat', 'jeea', 'eer', 'ej',
+          'jep', 'jel', 'are',
+          'jme', 'red', 'rand', 'rand', 'jole', 'jie', 'jpube', 'jde',
+          'jeh', 'ehr', 'eeh',
+          ]
+    journals = st.multiselect("Journals", js, js[:17])
     c1, c2, c3, c4 = st.beta_columns(4)
-    year_begin = c1.number_input('Year from', value=1980, min_value=1950, max_value=2020)
-    year_end = c2.number_input('Year to', value=2020, min_value=1950, max_value=2021)
-    sort_mth = c3.selectbox('Sort by', ['Most recent', 'Most cited'], index=0)
+    year_begin = c1.number_input('Year from', value=1980, min_value=year_min, max_value=year_max)
+    year_end = c2.number_input('Year to', value=year_max, min_value=year_min, max_value=year_max)
+    sort_mth = c3.selectbox('Sort by', ['Most recent', ], index=0)  # 'Most cited'
     max_show = c4.number_input('Max. Shown', value=100, min_value=0, max_value=500)
     button_clicked = st.button("Search")
 
     data_load_state = st.empty()
 
-    df = load_data()
+    search_keywords(button_clicked, df, key_words, journals, year_begin, year_end, sort_mth, max_show, data_load_state)
 
-    search_keywords(key_words, journals, year_begin, year_end, sort_mth, max_show, data_load_state)
 
 if __name__ == '__main__':
     main()
-
