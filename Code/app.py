19002a645ad3ccf2acf0215c5fd1c57c9373eb38
alalalalaki <harlan.zhu@gmail.com>
2022-01-14 14:35:43 +0900

    It turns out this does not matter

--- Code/app.py
+++ Code/app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title=None, page_icon=None, layout='centered', initial_s
 """
 
 
-# @st.cache(show_spinner=False)
+@st.cache(show_spinner=False)
 def load_data_cached(timestamp):
     df = pd.read_csv("Data/papers.csv",
                      usecols=["title", "authors", "abstract", "url",  "journal", "year"],  # "jel",
