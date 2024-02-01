e31094031ffcbd988b11131aee18c0bdb22ca9b7
alalalalaki <akaguro.koyomi@gmail.com>
2024-02-01 13:03:39 +0900

    Monthly data update.

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ cd ../Econ-Paper-Scrape/
 git pull
 
 # move data file
-cp Data/papers_recent.csv ../Econ-Paper-Search/Data/
+cp Data/papers_2020s.csv ../Econ-Paper-Search/Data/
 
 # commit and push in Econ-Paper-Search
 cd ../Econ-Paper-Search
