dac1ecc2a00dc624e9aa619a8539c11d0287f98a
alalalalaki <akaguro.koyomi@gmail.com>
2025-01-03 02:27:28 +0900

    Use automatic code for year_max

--- Code/app.py
+++ Code/app.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import os
 import re
+from datetime import datetime
+
 
 
 st.set_page_config(page_title=None, page_icon=None, layout='centered', initial_sidebar_state='collapsed')
@@ -344,7 +346,7 @@ def main():
         journals = set(journals)
 
     year_min = 1900
-    year_max = 2024
+    year_max = datetime.now().year
 
     c1, c2, c3, c4 = form.columns(4)
     year_begin = c1.number_input('Year from', value=1980, min_value=year_min, max_value=year_max)
